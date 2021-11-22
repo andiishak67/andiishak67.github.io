529b9859b43bbee79346939d2b9ca85b0b07e8f4
andiishak67 <91652502+andiishak67@users.noreply.github.com>
2021-11-22 22:32:31 +0800

    tugas8

--- movie8/js/tmdb.js
+++ movie8/js/tmdb.js
@@ -1,17 +1,18 @@
 const key = "f3091daf1f73c337183a13decb80dc0e"; 
 const endpoint_url = 'https://api.themoviedb.org/3/';
 
-function getListMovie() {
-    fetch(endpoint_url + "movie/now_playing?api_key=" + key + "&language=en-US&page=1")
+function getListMovie( services, sectiontitle) {
+    fetch(endpoint_url + services + "?api_key=" + key + "&language=en-US&page=1")
     .then(status)
     .then(json)
     .then(function(data) {
 
     var moviesHTML = "";
     data.results.forEach(function(movie) {
-    moviesHTML += `
+    moviesHTML = moviesHTML +`
         <div class="col m3 s6">
-            <div class="card">
+          
+            <div class="card moviecard">
                 <a href="./movie.html?id=${movie.id}">
                     <div class="card-image waves-effect waves-block waves-light">
                         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
@@ -19,6 +20,7 @@ function getListMovie() {
                 </a>
                 <div class="card-content text-center">
                     <strong>${movie.title}</strong>
+                    <strong>Release Date : ${movie.release_date}</strong>
                 </div>
             </div>
         </div>
@@ -26,6 +28,8 @@ function getListMovie() {
     });
 
     document.getElementById("movie_list").innerHTML = moviesHTML;
+    document.getElementById("section_title").innerHTML = sectiontitle;
+
     })
     .catch(error);
 }
